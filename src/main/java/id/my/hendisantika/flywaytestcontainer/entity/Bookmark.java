3cfd36f957cd4c26654b59923456ad09f14b5974
hendisantika <hendisantika@yahoo.co.id>
2025-02-17 10:48:31 +0700

    feat: Add Bookmark Entity 🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/flywaytestcontainer/entity/Bookmark.java
@@ -0,0 +1,59 @@
+package id.my.hendisantika.flywaytestcontainer.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-flyway-testcontainer
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 17/02/25
+ * Time: 10.47
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@Entity
+@Table(name = "bookmarks")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Bookmark {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "bookmarks_id_gen")
+    @SequenceGenerator(name = "bookmarks_id_gen", sequenceName = "bookmark_id_seq")
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Size(max = 200)
+    @NotNull
+    @Column(name = "title", nullable = false, length = 200)
+    private String title;
+
+    @Size(max = 500)
+    @NotNull
+    @Column(name = "url", nullable = false, length = 500)
+    private String url;
+
+    @NotNull
+    @ColumnDefault("now()")
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+}
