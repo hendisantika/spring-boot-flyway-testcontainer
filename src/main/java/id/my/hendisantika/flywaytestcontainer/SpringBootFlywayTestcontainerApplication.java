12e9760f1cdc67521b354bc63043deb680dad572
hendisantika <hendisantika@yahoo.co.id>
2025-02-17 10:41:42 +0700

    feat: Init Project 🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/flywaytestcontainer/SpringBootFlywayTestcontainerApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.flywaytestcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFlywayTestcontainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFlywayTestcontainerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/flywaytestcontainer/SpringBootFlywayTestcontainerApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.flywaytestcontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFlywayTestcontainerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
